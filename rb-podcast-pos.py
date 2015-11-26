c669537d93b851e700337ff5653d18e35e0aed1f
Ed Bruck <ed.bruck1@gmail.com>
2015-11-26 12:40:44 -0800

    * added notifications for debugging * added tracking which hosts played an episode * purge will only remove the entry if all hosts have deleted the episode

--- rb-podcast-pos.py
+++ rb-podcast-pos.py
@@ -22,12 +22,12 @@ import json
 
 REDIS_SERVER = "redis"
 REDIS_PORT = 6379
-REDIS_TIMEOUT = 10
+REDIS_TIMEOUT = 30
 RB_PODCAST_POS_SAVE_INTERVAL = 60
 SECONDS_IN_DAY = 86400
 PURGE_TIME = 30 * SECONDS_IN_DAY
 
-from gi.repository import GObject, Peas
+from gi.repository import GObject, Peas, Notify
 from gi.repository import RB
 
 
@@ -38,14 +38,16 @@ class PodcastPos(GObject.Object, Peas.Activatable):
 
     def __init__(self):
         GObject.Object.__init__(self)
+        Notify.init("PodcastPosPlugin")
 
         self.backup_file = os.path.expanduser('~') + '/.rb-podcast-pos.json'
-
         self.last_save = int(time.time())
+        self.hostname = socket.gethostname()
 
         try:
             redis_conn = redis.Redis(REDIS_SERVER, socket_timeout=REDIS_TIMEOUT, port=REDIS_PORT)
             self.pos_dict = json.loads(redis_conn.get('rb-podcast-pos:data').decode('utf8'))
+            Notify.Notification.new("Loaded " + str(len(self.pos_dict)) + " entries from redis").show()
         except:
             try:
                 self.pos_dict = json.load(open(self.backup_file))
@@ -57,11 +59,14 @@ class PodcastPos(GObject.Object, Peas.Activatable):
         now = int(time.time())
 
         for key in self.pos_dict:
+            # todo: remove the time check and only use for orphaned files after a lengthy interval
             if now - self.pos_dict[key]['timestamp'] >= PURGE_TIME:
-                if not os.path.isfile(urllib.parse.unquote(key[7:])):
-                    to_purge.append(key)
-                else:
-                    self.pos_dict[key]['timestamp'] = now
+                if self.hostname in self.pos_dict[key]['hosts']:
+                    if not os.path.isfile(urllib.parse.unquote(key[7:])):
+                        self.pos_dict[key]['hosts'].remove(self.hostname)
+                        # all hosts are gone, so we purge this entry
+                        if (len(self.pos_dict[key]['hosts']) == 0):
+                            to_purge.append(key)
 
         if len(to_purge):
             try:
@@ -97,6 +102,8 @@ class PodcastPos(GObject.Object, Peas.Activatable):
     def do_deactivate(self):
         self.purge_missing_and_save()
 
+        Notify.uninit()
+
         shell = self.object
         self.psc_id1 = None
         self.psc_id2 = None
@@ -116,6 +123,10 @@ class PodcastPos(GObject.Object, Peas.Activatable):
             song_info = self.get_song_info(entry)
             if song_info['location'] in self.pos_dict:
                 new_pos = self.pos_dict[song_info['location']]['pos']
+
+                # add new host
+                if self.hostname not in self.pos_dict[song_info['location']]['hosts']:
+                    self.pos_dict[song_info['location']]['hosts'].append(self.hostname)
 
                 if new_pos >= song_info['duration'] - 1:
                     return
@@ -140,7 +151,7 @@ class PodcastPos(GObject.Object, Peas.Activatable):
                     now = int(time.time())
 
                     if song_info['location'] not in self.pos_dict:
-                        self.pos_dict[song_info['location']] = {'timestamp': now, 'pos': pos}
+                        self.pos_dict[song_info['location']] = {'timestamp': now, 'pos': pos, 'hosts' : [self.hostname]}
                     else:
                         self.pos_dict[song_info['location']]['pos'] = pos
 
@@ -155,7 +166,8 @@ class PodcastPos(GObject.Object, Peas.Activatable):
             redis_conn = redis.Redis(REDIS_SERVER, socket_timeout=REDIS_TIMEOUT, port=REDIS_PORT)
             redis_conn.set('rb-podcast-pos:data', json_data)
             redis_conn.set('rb-podcast-pos:log',
-                           socket.gethostname() + '@' + time.strftime('%c') + ', count=' + str(len(self.pos_dict)))
+                           self.hostname + '@' + time.strftime('%c') + ', count=' + str(len(self.pos_dict)))
+            Notify.Notification.new("Saved " + str(len(self.pos_dict)) + " entries to redis").show()
         except:
             pass
 
